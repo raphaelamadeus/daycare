1065eb6a5265f3dcca9ae51080a6cd2bae0d0716
Swishy <super.quazimodo@gmail.com>
2013-02-16 19:44:40 +1100

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Daycare::Application.config.secret_token = 'e91245b81e57d31a288fb7ead7c81d30e411c8acd1c4cd93c50a1322a4ff99bc405eed051c8124dc7a70b5cf21db3ec338c34b2e2167589fd09c15beba267530'
---
+++ config/initializers/version.rb
@@ -0,0 +1,5 @@
+module Daycare
+  class Application
+    VERSION = "0.1"
+  end
+end
